b13280fcdcca3b92283124864e6ba9fd25917452
Mathieu Mérino <mmerino@outlook.fr>
2017-11-03 01:46:11 +0100

    [wip]

--- cmd/scenario_2/scenario_2_backend/main.go
+++ cmd/scenario_2/scenario_2_backend/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"../../../src/wifi"
+	"../../../src/feu"
+)
+
+func main(){
+	shutdownWifi := make(chan struct{})
+
+	go wifi.StartWifi(shutdownWifi)
+	go feu.NewFeu("localhost:1234")
+
+	select { }
+}
--- cmd/scenario_2/scenario_2_demo/main.go
+++ cmd/scenario_2/scenario_2_demo/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"../../../src/voiture"
+	"time"
+)
+func main(){
+	mat := voiture.Materiel{
+		ID: 2,
+		Vitesse: 100,
+		Position: voiture.Position{X: 100},
+		Frein: false,
+	}
+
+	conducteur := voiture.ConducteurLog{}
+	go voiture.NewVoiture("localhost:1234", &mat, conducteur, time.After, make(chan struct{}))
+
+
+}
