429096abe2412419e22870fd6c8c7176830fc7b5
Mathieu Mérino <mmerino@outlook.fr>
2018-01-18 18:06:53 +0100

    configuration for threads to work

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,9 +1,33 @@
 #include "controller.hpp"
-#include <iostream>
+#include <thread>
+
+void start_controller()
+{
+	Controller controller;
+	controller.ControllerLoop();
+}
+
+void start_wifi_broadcaster()
+{
+
+}
+
+void start_wifi_receiver()
+{
+
+}
+
+void start_car_interface()
+{
+
+}
 
 int main(){
+	std::thread threadController(start_controller);
     Controller controller;
 	controller.ControllerLoop();
+
+	threadController.join();
 
 	return 0;
 }
