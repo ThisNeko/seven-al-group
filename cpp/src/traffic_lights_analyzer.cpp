e42d72d6644d2eedc4283a9a55e184009d1b3424
Gui <fire-fag@hotmail.fr>
2018-02-12 04:56:15 +0100

    algo feu cpp

--- cpp/src/traffic_lights_analyzer.cpp
+++ cpp/src/traffic_lights_analyzer.cpp
@@ -7,7 +7,7 @@ TrafficLightStatus* SelectTrafficLight(std::map <int, TrafficLightStatus> &traff
 
     for (auto it = trafficLightsRegistry.begin(); it != trafficLightsRegistry.end(); ++it)
     {
-        selected = &(it->second);
+        selected = &(it->second);        
     }
 
     if (selected == nullptr)
@@ -16,4 +16,43 @@ TrafficLightStatus* SelectTrafficLight(std::map <int, TrafficLightStatus> &traff
     }
 
     return selected;
+}
+
+
+double calculVitesse(TrafficLightStatus* tl, const CarStatus &carStatus){
+
+    Vitesse vitesseVoiture = carStatus.vitesse;
+    int tempsEcoule = tl->ticker;
+    int tempsFeuTotal = tl->timer;
+    int X1 = tl->pos.X;
+    int Y1 = tl->pos.Y;
+    int X2 = carStatus.position.X;
+    int Y2 = carStatus.position.Y;
+
+    double distanceFeuVoiture =  sqrt(pow(X2-X1,2)+pow(Y2-Y1,2));
+
+    double tempsVoitureArriveFeu = (distanceFeuVoiture)/(vitesseVoiture.X/3.6);
+
+    int modulo = fmod(tempsVoitureArriveFeu+double(tempsEcoule),2*tempsFeuTotal);
+
+    if(tl->couleur==RED){
+        if(modulo<=tempsFeuTotal){
+            int nouvelleVitesse = int(distanceFeuVoiture)/int((tempsVoitureArriveFeu + (tempsFeuTotal - modulo)));
+            //carStatus.
+            return (double)nouvelleVitesse*3.6;
+        } else {
+
+        }
+    } else {
+        if(modulo<=tempsFeuTotal){
+
+        } else {
+            int nouvelleVitesse = int(distanceFeuVoiture)/int(tempsVoitureArriveFeu+ (2*tempsFeuTotal-(modulo+1)));
+            return (double)nouvelleVitesse*3.6;
+        }
+    }
+
+
+
+
 }
--- cpp/src/traffic_lights_analyzer.hpp
+++ cpp/src/traffic_lights_analyzer.hpp
@@ -4,7 +4,9 @@
 #include <map>
 #include "structs/traffic_light_status.hpp"
 #include "structs/car_status.hpp"
+#include <cmath>
 
 TrafficLightStatus* SelectTrafficLight(std::map <int, TrafficLightStatus> &trafficLightsRegistry, const CarStatus &carStatus);
+double calculVitesse(TrafficLightStatus* tl, const CarStatus &carStatus);
 
 #endif // TRAFFIC_LIGHTS_ANALYZER_H
